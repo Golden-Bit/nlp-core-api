8e76072f759a758ad66f9b162827935cde128fe3
sans <simonesansalone777@gmail.com>
2024-12-01 18:42:25 +0100

    update report tools

--- chains/chain_scripts/utilities/report.py
+++ chains/chain_scripts/utilities/report.py
@@ -339,7 +339,8 @@ class TemplateManager:
 
     <!-- Footer -->
     <footer>
-        <p>&copy; 2024 {company_name} - Tutti i diritti riservati</p>
+        <p>&copy; 2024 Bluen s.r.l. - Tutti i diritti riservati</p>
+        <img src="https://static.wixstatic.com/media/63b1fb_3e137ee583724e568424f13468c4d92d~mv2.png/v1/fill/w_434,h_95,al_c,lg_1,q_85,enc_avif,quality_auto/Cattura-BLUEN_PNG_edited.png" alt="Logo" style="position: absolute; top: 20px; right: 20px; height: 50px;">
     </footer>
 </body>
 </html>
