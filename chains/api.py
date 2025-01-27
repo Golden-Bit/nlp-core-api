643302ccce75a8dc7d3b6871e914350f8e18a7dc
sans <simonesansalone777@gmail.com>
2025-01-27 12:17:39 +0100

    sync remote with local changes

--- chains/api.py
+++ chains/api.py
@@ -12,7 +12,8 @@ from pymongo import MongoClient
 from chains.utilities.chain_manager import ChainManager
 from fastapi.responses import StreamingResponse
 
-from langchain_community.callbacks.manager import get_openai_callback
+#from langchain_community.callbacks.manager import get_openai_callback
+from langchain_community.callbacks import get_openai_callback
 
 router = APIRouter()
 MONGO_CONNECTION_STRING = os.getenv('MONGO_CONNECTION_STRING', 'localhost')
